30a249d73ca04dc17d7040bde126ad2a797dae8d
moon <flbrazhnikov@gmail.com>
2024-08-08 01:36:12 +0300

    change config

--- src/embeddings-api/config.py
+++ src/embeddings-api/config.py
@@ -1,16 +1,15 @@
 import pathlib
-from functools import lru_cache
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    device: str
+    vectorization_model_path: pathlib.Path
+    tokenizer_path: pathlib.Path
+
     model_config = SettingsConfigDict(env_file='.env')
 
-    vec_model_path: pathlib.Path
-    max_workers: int
 
-
-@lru_cache()
-def get_settings() -> Settings:
-    return Settings()
+def get_settings() -> dict:
+    return Settings().model_dump()
